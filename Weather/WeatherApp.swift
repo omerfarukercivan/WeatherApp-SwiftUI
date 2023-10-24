44f8d3d65859fdb35c4d6c03481349669cd9a400
Ömer Faruk Ercivan <ofar38@gmail.com>
2023-10-24 13:39:41 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ömer Faruk Ercivan on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
